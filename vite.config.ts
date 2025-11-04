789aa19780e8b73d1dddbb02cfb0150ca6c07bd0
Lim Ji Sun <time2_go@naver.com>
2025-11-04 17:54:45 +0900

    메신저 페이지연결

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/Umessenger/',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // 👈 요 줄 추가!
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
