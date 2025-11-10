21eeef285fe91eb082bbebfece67234a1ab59241
Lim Ji Sun <time2_go@naver.com>
2025-11-10 17:31:27 +0900

    로그인 디자인수정 및 조직도 수정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,32 @@
+import { useEffect } from 'react';
 import { Button, Input, Form } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Checkbox } from '../components/common';
 import { useAuthStore } from '../store/authStore';
 import type { LoginCredentials } from '../types';
 import styles from '../styles/Login.module.css';
+import logo from '../assets/images/m_logo.png';
 
 const Login = () => {
   const login = useAuthStore((state) => state.login);
   const [form] = Form.useForm();
 
-  const onSubmit = (values: LoginCredentials) => {
+  // 저장된 아이디 불러오기
+  useEffect(() => {
+    const savedUsername = localStorage.getItem('rememberedUsername');
+    if (savedUsername) {
+      form.setFieldsValue({ username: savedUsername, remember: true });
+    }
+  }, [form]);
+
+  const onSubmit = (values: LoginCredentials & { remember?: boolean }) => {
+    // 기억하기 체크되어 있으면 localStorage에 저장
+    if (values.remember) {
+      localStorage.setItem('rememberedUsername', values.username);
+    } else {
+      localStorage.removeItem('rememberedUsername');
+    }
+
     const mockUser = {
       id: '1',
       username: values.username,
@@ -30,42 +48,56 @@ const Login = () => {
 
   return (
     <div className={styles.loginContainer}>
-      <div className={styles.loginBox}>
-        <div className={styles.loginHeader}>
-          <h1>메신저 로그인</h1>
-          <p>계정 정보를 입력해주세요</p>
-        </div>
-
-        <Form form={form} onFinish={onSubmit} className={styles.loginForm}>
-          <Form.Item
-            name="username"
-            rules={[{ required: true, message: '아이디를 입력해주세요' }]}
-          >
-            <Input
-              size="large"
-              placeholder="아이디"
-              prefix={<UserOutlined />}
-            />
-          </Form.Item>
-
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: '비밀번호를 입력해주세요' }]}
-          >
-            <Input.Password
-              size="large"
-              placeholder="비밀번호"
-              prefix={<LockOutlined />}
-            />
-          </Form.Item>
-
-          <Form.Item>
-            <Button type="primary" htmlType="submit" size="large" block>
-              로그인
-            </Button>
-          </Form.Item>
-        </Form>
+      
+      <div className={styles.loginlogo}>
+        <img
+            src={logo}
+            alt="umessenger"
+          />
       </div>
+      <div className={styles.loginBoxWrap}>
+        <div className={styles.loginBox}>
+          <Form form={form} onFinish={onSubmit} className={styles.loginForm}>
+            <div className={styles.loginInput}>
+              <Form.Item
+                name="username"
+              >
+                <Input
+                  placeholder="아이디를 입력하세요"
+                />
+              </Form.Item>
+
+              <Form.Item
+                name="password"
+              >
+                <Input.Password
+                  placeholder="비밀번호를 입력하세요"
+                />
+              </Form.Item>
+
+              <Form.Item
+                name="remember"
+                valuePropName="checked"
+                className={styles.rememberItem}
+              >
+                <Checkbox
+                  checked={form.getFieldValue('remember') || false}
+                  onChange={(checked) => form.setFieldValue('remember', checked)}
+                  label="기억하기"
+                  type="checkbox"
+                />
+              </Form.Item>
+           </div>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit" size="large" block>
+                로그인
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+      <p className={styles.loginfooter}>© 2025 NEXUS COMMUNITY All rights reserved.</p>
     </div>
   );
 };
